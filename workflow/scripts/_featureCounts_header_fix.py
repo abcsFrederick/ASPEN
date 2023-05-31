04d65f35c13af869d64edc02693ba30ffa2ad012
kopardev <vishal.koparde@nih.gov>
2023-05-31 11:57:13 -0400

    new script for featurecounts rule

---
+++ workflow/scripts/_featureCounts_header_fix.py
@@ -0,0 +1,19 @@
+import os,sys
+
+i=0
+for l in sys.stdin:
+	i+=1
+	if i==1: continue
+	if i==2:
+		outlist=[]
+		l=l.strip().split("\t")
+		for s in l:
+			if s.startswith("/"):
+				x=os.path.basename(s)
+				x=x.replace(".genrich.tn5nicks.bam","")
+				outlist.append(x)
+			else:
+				outlist.append(s)
+		print("\t".join(outlist))
+	else:
+		print(l.strip())
