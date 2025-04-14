fc7e892b53919dc0864f79971136074041763e87
kopardev <vishal.koparde@nih.gov>
2025-04-14 15:29:05 -0400

    refact: FRIP is calculated from dedupBAM for each peak caller

--- workflow/scripts/ccbr_frip.bash
+++ workflow/scripts/ccbr_frip.bash
@@ -7,8 +7,8 @@ source <(curl -s https://raw.githubusercontent.com/CCBR/Tools/master/scripts/arg
 exit 1
 argparse "$@" <<EOF || exit 1
 parser.add_argument('--narrowpeak',required=True, help='input narrowPeak or bed file')
-parser.add_argument('--peakcaller',required=False, default="", help='peakcaller')
-parser.add_argument('--tagalign',required=True, help='input tagAlign.gz or bed.gz file')
+parser.add_argument('--peakcaller',required=False, default="", help='peakcaller either Genrich or MACS2')
+parser.add_argument('--dedupbam',required=True, help='input deduplicated bam file')
 parser.add_argument('--samplename',required=True, help='sample name')
 parser.add_argument('--dhsbed',required=False, help='known DHS sites')
 parser.add_argument('--promoterbed',required=False, help='known Promoter sites')
@@ -16,15 +16,17 @@ parser.add_argument('--enhancerbed',required=False, help='known Enhancer sites')
 EOF
 
 set -e -x -o pipefail
-nreads=$(zcat $TAGALIGN|wc -l)
+nreads=$(samtools view -c $DEDUPBAM)
 
-frip_reads=$(bedtools intersect -wa -a $TAGALIGN -b $NARROWPEAK|wc -l)
+frip_reads=$(bedtools intersect -u -a $DEDUPBAM -b $NARROWPEAK|samtools view -c)
 frip=$(echo "scale=3;$frip_reads/$nreads"|bc)
-echo -ne "$SAMPLENAME\tFRiP${PEAKCALLER}\t$frip\n"
+echo -ne "$SAMPLENAME\tFRiP\t${PEAKCALLER}\t$frip\n"
+
+if [ "$PEAKCALLER" == "Genrich" ];then
 
 if [ $DHSBED ];then
 if [ "$DHSBED" != "" ];then
-fridhs_reads=$(bedtools intersect -wa -a $TAGALIGN -b $DHSBED|wc -l)
+fridhs_reads=$(bedtools intersect -u -a $DEDUPBAM -b $DHSBED|samtools view -c)
 fridhs=$(echo "scale=3;$fridhs_reads/$nreads"|bc)
 echo -ne "$SAMPLENAME\tFRiDHS\t$fridhs\n"
 fi
@@ -32,7 +34,7 @@ fi
 
 if [ $PROMOTERBED ];then
 if [ "$PROMOTERBED" != "" ];then
-fripro_reads=$(bedtools intersect -wa -a $TAGALIGN -b $PROMOTERBED|wc -l)
+fripro_reads=$(bedtools intersect -u -a $DEDUPBAM -b $PROMOTERBED|samtools view -c)
 fripro=$(echo "scale=3;$fripro_reads/$nreads"|bc)
 echo -ne "$SAMPLENAME\tFRiPro\t$fripro\n"
 fi
@@ -40,8 +42,10 @@ fi
 
 if [ $ENHANCERBED ];then
 if [ "$ENHANCEDBED" != "" ];then
-frienh_reads=$(bedtools intersect -wa -a $TAGALIGN -b $ENHANCERBED|wc -l)
+frienh_reads=$(bedtools intersect -u -a $DEDUPBAM -b $ENHANCEDBED|samtools view -c)
 frienh=$(echo "scale=3;$frienh_reads/$nreads"|bc)
 echo -ne "$SAMPLENAME\tFRiEnh\t$frienh\n"
 fi
+fi
+
 fi
